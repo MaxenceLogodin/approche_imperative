ebeb9db5a5ab910ec1f8150fd26b0900cf361845
Maxence LOGODIN <maxence.logodin@gmail.com>
2022-10-06 15:36:05 +0200

    Init

---
+++ src/diginamic/Bonjour.java
@@ -0,0 +1,12 @@
+package diginamic;
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		System.out.println("Bonjour tout le monde");
+		
+		int a = 053;
+		System.out.println(a);
+	}
+
+}
